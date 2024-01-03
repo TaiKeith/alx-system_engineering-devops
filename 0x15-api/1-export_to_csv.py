5e79f8bd716089167501eaf31f527dafd1253595
Tai Keith <keithsteve.ks@hotmail.com>
2024-01-03 03:02:17 -0800

    Export to CSV

---
+++ 0x15-api/1-export_to_csv.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This script uses a REST API for a given employee ID and returns information
+about his/her TODO list progress and exports the data in CSV format
+"""
+from requests import get
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    response = get(url)
+    username = response.json().get("username")
+
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
+    response = get(url)
+    tasks = response.json()
+
+    with open(f"{user_id}.csv", "w", newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(['User_ID', 'Username', 'Completed', 'Title'])
+
+        for task in tasks:
+            csv_writer.writerow([user_id, username, task.get("completed"),
+                task.get("title")])
